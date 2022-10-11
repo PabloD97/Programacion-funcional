75f359f7816ff38d64f6a5e7cd36d9241d24c8c2
Pablo Diaz <pablodiaz@techforb.com>
2022-10-11 16:52:54 -0300

    New commit

--- practica/practica2.hs
+++ practica/practica2.hs
@@ -4,6 +4,7 @@ apply f = g
 
 first (x,y) = x
 
+second (x, y) = y
 swap (x,y) = (y,x)
 
 uflip :: ((a,b) -> c) -> (b,a) -> c
@@ -34,3 +35,82 @@ appPar (f, g) = h
 appDist f = g 
   where g (x, y) = (f x, f y)  
 
+compose :: (b->c) -> (a->b) ->a -> c
+compose f g x = f (g x) 
+
+id x = x
+
+flip f x y = (f y) x
+
+
+data Gusto = Chocolate | DulceDeLeche | Frutilla | Sambayon
+data Helado = Vasito Gusto | Cucurucho Gusto Gusto |Pote Gusto Gusto Gusto
+
+chocoHelate consH = consH Chocolate
+
+data DigBin = O | I
+
+dbAsInt :: DigBin -> Int
+dbAsInt O = 0
+dbAsInt I = 1
+
+data DigDec = D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8 | D9
+
+data Medida = Mm Float | Cm Float |  Inch Float | Foot Float deriving (Eq, Show)
+
+asMm :: Medida -> Medida
+asMm (Cm n) = Mm (n * 10)
+asMm (Inch n) = Mm (n * 25.4)
+asMm (Foot n) = Mm (n * 25.4)
+asMm (Mm n) = Mm n
+
+asCm :: Medida -> Medida 
+asCm (Mm n) = Cm (n / 10)
+asCm (Inch n) = Cm (n * 2.54)
+asCm (Mm n) = Cm (n * 2.54)
+asCm (Cm n) = Cm n
+
+asInch :: Medida -> Medida
+asInch (Inch n) = Inch n
+asInch (Cm n) = Inch (n / 2.53)
+asInch (Mm n) = Inch (n / 25.3)
+asInch (Foot n) = Inch n
+
+curry :: ((a,b) -> c) -> a -> b -> c
+curry f x y = f(x,y)
+
+uncurry :: (a -> b -> c) -> (a,b) -> c
+uncurry f(x,y)= f x y
+
+
+data Shape = Circle Float | Rect Float Float deriving (Eq, Show)
+
+construyeShNormal :: (Float -> Shape) -> Shape
+construyeShNormal c = c 1.0
+
+-- Ejercicio 7)
+data MayFail a = Raise Exception | Ok a
+data Exception = DivByZero | NotFound | NullPointer | Other String
+type ExHandler a = Exception -> a
+
+
+-- definir la función
+--tryCatch :: MayFail a -> (a -> b) -> ExHandler b -> b
+
+
+-- Ejmeplo de uso:
+
+-- sueldoGUIE :: Nombre -> [Empleado] -> GUI Int
+-- sueldoGUIE nombre empleados =
+--     tryCatch (lookupE nombre empleados) mostrarInt
+--        (\e -> case e of
+--                NotFound -> ventanaError msgNotEmployee
+--                       _ -> error msgUnexpected)
+--     where msgNotEmployee = "No es empleado de la empresa"
+--     msgUnexpected = "Error inesperado"
+
+-- sabiendo que:
+
+-- mostrarInt :: Int -> GUI Int
+-- ventanaError :: String -> GUI a
+-- lookupE :: Nombre -> [Empleado] -> MayFail Int
---
+++ practica/practica7.hs
@@ -0,0 +1,190 @@
+-- 1)
+data Pizza = Prepizza | Capa Ingrediente Pizza  deriving (Eq, Show)
+data Ingrediente = Aceitunas Int| Anchoas |Cebolla|Jamon | Queso |Salsa  deriving (Eq, Show)
+ 
+-- 2) 
+-- f Prepizza = 
+-- f (Capa ing p) = ... f p ...
+-- Si no sigue lo de arriba, esta mal demostrada la recursion estrutural.
+cantidadDeCapas :: Pizza -> Int
+cantidadDeCapas Prepizza = 0
+cantidadDeCapas (Capa ing p) = 1 + cantidadDeCapas p
+
+-- Meter un if aca, es complicarla. Si lo metemos, cuando hagamos 
+-- la demostracio, vamos a tener que hacerlo por casos.
+-- A nivel codigo no es mala,pero a nivel demostracion si lo es.
+cantidadDeAceitunas :: Pizza -> Int
+cantidadDeAceitunas Prepizza = 0
+cantidadDeAceitunas (Capa ing p) = aceitunas ing + cantidadDeAceitunas p
+
+aceitunas :: Ingrediente -> Int
+aceitunas (Aceitunas n) = n
+aceitunas _				= 0
+
+---------------
+
+duplicarAceitunas :: Pizza -> Pizza
+duplicarAceitunas Prepizza 		= Prepizza
+duplicarAceitunas (Capa ing p)  =  
+	Capa (aceitunasPorDos ing) (duplicarAceitunas p)
+
+aceitunasPorDos :: Ingrediente -> Ingrediente
+aceitunasPorDos (Aceitunas n) = Aceitunas (n*2)
+aceitunasPorDos ing			  = ing
+-- Igualito a la funcion 'id' esto. Cuando nos viene algo que no queremos
+-- hacer nada, lo devolvemos como vino.
+
+---------------
+-- Como un filter
+sinLactosa :: Pizza -> Pizza
+sinLactosa Prepizza 	= Prepizza
+sinLactosa (Capa ing p) = 
+	sacarQueso ing (sinLactosa p)
+
+sacarQueso :: Ingrediente -> Pizza -> Pizza
+sacarQueso Queso p = p
+sacarQueso ing p   = Capa ing p
+
+-- sacarQueso :: Ingrediente -> (Pizza -> Pizza)
+-- sacarQueso Queso = id
+-- sacarQueso ing   = Capa ing 
+-- Esta es equivalente a la de arriba D:
+
+--------------------
+aptaIntolerantesLactosa :: Pizza -> Bool
+aptaIntolerantesLactosa Prepizza = True
+aptaIntolerantesLactosa (Capa ing p) =
+	not (esQueso ing) && aptaIntolerantesLactosa p
+
+-- Es bueno que las funciones siempre sean en afirmativo
+esQueso :: Ingrediente -> Bool
+esQueso Queso = False
+esQueso _ = True
+
+--------------------
+-- normilizacion -> tengo pizzas similares y las convierto en una
+conDescripcionMejorada :: Pizza -> Pizza 
+conDescripcionMejorada Prepizza 	= Prepizza
+conDescripcionMejorada (Capa ing p) = 
+	comprimirAceitunas ing (conDescripcionMejorada p)
+
+comprimirAceitunas :: Ingrediente -> Pizza -> Pizza
+comprimirAceitunas (Aceitunas n) (Capa (Aceitunas m) p) = 
+	Capa (Aceitunas (n+m)) p
+comprimirAceitunas ing p = Capa ing p
+---------------------
+
+
+
+-- SECCION II
+type Nombre = String
+data Planilla = Fin | Registro Nombre Planilla
+data Equipo = Becario Nombre | Investigador Nombre Equipo Equipo Equipo
+
+largoDePlanilla :: Planilla -> Int
+largoDePlanilla Fin = 0
+largoDePlanilla (Registro n p) = 1 + largoDePlanilla p 
+
+esta :: Nombre -> Planilla -> Bool
+esta nombre Fin = False
+esta nombre (Registro n p) = nombre == n || esta p
+
+juntarPlanillas :: Planilla -> Planilla -> Planilla
+juntarPlanillas Fin p2 = p2
+juntarPlanillas (Registro n p) p2 = Registro n (juntarPlanillas p p2)
+
+nivelesJerarquicos :: Equipo -> Int
+nivelesJerarquicos (Becario n) = 0
+nivelesJerarquicos (Investigador n e1 e2 e3) = 
+	1 +
+	nivelesJerarquicos e1 
+	`max`
+	nivelesJerarquicos e2
+	`max`
+	nivelesJerarquicos e3
+
+cantidadDeIntegrantes :: Equipo -> Int
+cantidadDeIntegrantes (Becario n) = 1
+cantidadDeIntegrantes (Investigador n e1 e2 e3) = 
+	1 +
+	cantidadDeIntegrantes e1 +
+	cantidadDeIntegrantes e2 +
+	cantidadDeIntegrantes e3
+
+planillaDeIntegrantes :: Equipo -> Int
+planillaDeIntegrantes (Becario n) = Registro n Fin
+planillaDeIntegrantes (Investigador n e1 e2 e3) = 
+	Registro n 
+	(
+		planillaDeIntegrantes e1 
+		`juntarPlanillas`
+		planillaDeIntegrantes e2 
+		`juntarPlanillas`
+		planillaDeIntegrantes e3
+	)
+
+data Dungeon a = Habitacion a 
+				| Pasaje (Maybe a) (Dungeon a)	
+				| Bifurcacion (Maybe a) (Dungeon a) (Dungeon a)
+
+
+-- f (Habitacion a) 	= 
+-- f (Pasaje m d)  	= ... f d
+-- f (Bifurcacion m d1 d2) = ... f d1 ... f d2
+
+cantidadDeBifurcaciones :: Dungeon -> Int
+cantidadDeBifurcaciones (Habitacion a) 	= 0
+cantidadDeBifurcaciones (Pasaje m d)  	= 0 + cantidadDeBifurcaciones d
+cantidadDeBifurcaciones (Bifurcacion m d1 d2) = 
+	1 
+	+ cantidadDeBifurcaciones d1 
+	+ cantidadDeBifurcaciones d2
+
+cantidadDePuntosInteresantes :: Dungeon -> Int
+cantidadDePuntosInteresantes (Habitacion a) 	= 1
+cantidadDePuntosInteresantes (Pasaje m d)  	= unoSiJust m  + cantidadDePuntosInteresantes d
+cantidadDePuntosInteresantes (Bifurcacion m d1 d2) = 
+	unoSiJust m
+	+ cantidadDePuntosInteresantes d1 
+	+ cantidadDePuntosInteresantes d2
+
+-- case m of
+--	Nothing -> cantidadDePuntosInteresantes d
+-- 	Just x  -> 1 + cantidadDePuntosInteresantes d
+
+unoSiJust :: Maybe -> Int
+unoSiJust Nothing = 0
+unoSiJust (Just x) = 1
+
+cantidadDePuntosVacios :: Dungeon -> Int
+cantidadDePuntosVacios (Habitacion a) 	= 1
+cantidadDePuntosVacios (Pasaje m d)  	= unoSiEstaVacio m  + cantidadDePuntosVacios d
+cantidadDePuntosVacios (Bifurcacion m d1 d2) = 
+	unoSiEstaVacio m
+	+ cantidadDePuntosVacios d1 
+	+ cantidadDePuntosVacios d2
+
+-- case m of
+--	Nothing -> cantidadDePuntosInteresantes d
+-- 	Just x  -> 1 + cantidadDePuntosInteresantes d
+
+unoSiEstaVacio :: Maybe -> Int
+unoSiEstaVacio Nothing = 1
+unoSiEstaVacio (Just x) = 0
+
+
+esLineal :: Dungeon -> Int
+esLineal (Habitacion a) = True
+esLineal (Pasaje m d)  	= esLineal d
+esLineal (Bifurcacion m d1 d2) = False
+	-- No tiene sentido hacer esto
+	-- False && esLineal d1 && esLineal d2
+	-- El 'False' absorve lo demas.
+
+llenoDe :: Eq a => a -> Dungeon a -> Bool
+llenoDe e (Habitacion x)  	= e == x 
+llenoDe e (Pasaje m d)  	= llenoDe d
+llenoDe e (Bifurcacion m d1 d2) = False
+
+-- todo lo que esta del lado izquierdo de la flecha, se llama constraints
+-- o condicion que tiene que cumplir un tipo.
